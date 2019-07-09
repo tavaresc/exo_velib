f3adbdb5c1f10579e3caa78234b3a14d0db409d3
Claudia Tavares <claudia.tavares@fabernovel.com>
2019-07-09 18:45:16 +0200

    feat: add view for nearest stations

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -8,14 +8,7 @@ class StationsController < ApplicationController
     interactor = StationsInteractor.new
     #client = ClientsInteractor.new
     #client.fetch_and_save_stations
-    #all_stations = interactor.fetch_and_save_stations
-    #interactor.save_stations(all_stations)
-    # set_sizes(5, 10)
-    # data = Velib::RestClient.new.get_velib_api
-    # Velib::Repository.new.map_to_station(data, @number_of_stands)
-    #byebug
 
-    nearest_stations = interactor.find_nearest_stations(:latitude, :longitude)
-    puts nearest_stations
+    @stations = interactor.find_nearest_stations(@station.latitude, @station.longitude)
   end
 end
